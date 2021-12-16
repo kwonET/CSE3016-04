ba1f877978bc8269bef4d7e42dd9fde231ad66d4
dlxl3652zz@gmail.com <dlxl3652zz@gmail.com>
2021-12-16 23:50:49 +0900

    2주차

---
+++ 2week_inverter_buffer/버퍼제출용/inv.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/09/09 21:01:56
+// Design Name: 
+// Module Name: inv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module inv(
+    input a,
+    output y
+    );
+    
+assign y= a;
+endmodule
---
+++ 2week_inverter_buffer/버퍼제출용/inv_tb.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/09/09 21:04:09
+// Design Name: 
+// Module Name: inv_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module inv_tb;
+reg aa;
+wire y;
+
+inv u_inv(
+.a (aa ),
+.y (y ) );
+
+initial aa = 1'b0;
+always aa = #100 ~aa;
+
+initial begin
+    #1000
+    $finish;
+end
+
+endmodule
